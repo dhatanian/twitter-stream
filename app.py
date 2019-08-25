d1d1b2d946ddfcefc76a98a33d7f5aaf78f2fb39
David Hatanian <david@hatanian.com>
2019-08-25 11:43:45 +0100

    Go easy on Twitter API

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, copy_current_request_context
+from flask import Flask, render_template, copy_current_request_context, session
 from flask_socketio import SocketIO, emit
 from twython_stream import TwitterStream
 from eventlet.green import threading
 import os
 import eventlet
+import uuid
 
 #eventlet.monkey_patch(socket=False,thread=False)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
 socketio = SocketIO(app=app,async_mode='eventlet')
+
+consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+token = os.environ['TWITTER_TOKEN']
+token_secret = os.environ['TWITTER_TOKEN_SECRET']        
+stream = TwitterStream(consumer_key, consumer_secret, token, token_secret)
+
+threading.Thread(target=stream.statuses.sample, daemon=True).start()    
 
 @app.route('/')
 def index():
@@ -18,26 +27,21 @@ def index():
 @socketio.on('connect')
 def test_connect():
     @copy_current_request_context
-    def stream_tweets():
-        consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-        consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
-        token = os.environ['TWITTER_TOKEN']
-        token_secret = os.environ['TWITTER_TOKEN_SECRET']
-        stream = TwitterStream(consumer_key, consumer_secret, token, token_secret, consume_tweet)
-        stream.statuses.sample()
-    
     def consume_tweet(t):
-        if 'place' in t and t['place'] is not None:
-            print('Sending tweet')
-            emit('tweet', t['place'])
-        eventlet.sleep(0.001)
+        emit('tweet', t['place'])
+        
+    if('uid' not in session):
+        session['uid'] = uuid.uuid4()
 
+    stream.add_callback(session['uid'], consume_tweet)
     print('Client connected')
     emit('my response', {'data': 'Connected'})
-    threading.Thread(target=stream_tweets, daemon=True).start()    
+    
 
 @socketio.on('disconnect')
 def test_disconnect():
+    if('uid' in session):
+        stream.remove_callback(session['uid'])
     print('Client disconnected')
 
 if __name__ == '__main__':
--- twython_stream.py
+++ twython_stream.py
@@ -1,18 +1,30 @@
 from twython import TwythonStreamer
-
+import eventlet
 
 class TwitterStream(TwythonStreamer):
 
-    def __init__(self, consumer_key, consumer_secret, token, token_secret, callback):
-        self.callback = callback
+    def __init__(self, consumer_key, consumer_secret, token, token_secret):
+        self.callbacks = dict()
         super(TwitterStream, self).__init__(consumer_key, consumer_secret, token, token_secret)
 
-    def on_success(self, data):
-        self.callback(data)    
-            
+    def on_success(self, t):
+        if 'place' in t and t['place'] is not None:
+            print('Sending tweet')
+            for callback in self.callbacks.values():
+                callback(t)
+            eventlet.sleep(0.5)
+        eventlet.sleep(0.1)
+    
+    def add_callback(self, id, callback):
+        self.callbacks[id] = callback
+
+    def remove_callback(self, id):
+        del self.callbacks[id]
 
     def on_error(self, status_code, data):
         print(status_code)
+        if status_code==420:
+            eventlet.sleep(1)
         # Want to stop trying to get data because of the error?
         # Uncomment the next line!
         # self.disconnect()
