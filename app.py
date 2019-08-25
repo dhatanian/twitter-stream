f4e190d9c811ea0b3e0b454a9a2873127c5545ce
David Hatanian <david@hatanian.com>
2019-08-25 12:43:28 +0100

    Use SocketIO rooms

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, copy_current_request_context, session
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from twython_stream import TwitterStream
 from eventlet.green import threading
 import os
@@ -16,7 +16,13 @@ consumer_key = os.environ['TWITTER_CONSUMER_KEY']
 consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
 token = os.environ['TWITTER_TOKEN']
 token_secret = os.environ['TWITTER_TOKEN_SECRET']        
-stream = TwitterStream(consumer_key, consumer_secret, token, token_secret)
+
+def consume_tweet(t):
+    if 'place' in t and t['place'] is not None:
+        print('Sending tweet')
+        socketio.emit('tweet', t['place'], room='tweets')
+
+stream = TwitterStream(consumer_key, consumer_secret, token, token_secret, consume_tweet)
 
 threading.Thread(target=stream.statuses.sample, daemon=True).start()    
 
@@ -26,22 +32,13 @@ def index():
 
 @socketio.on('connect')
 def test_connect():
-    @copy_current_request_context
-    def consume_tweet(t):
-        emit('tweet', t['place'])
-        
-    if('uid' not in session):
-        session['uid'] = uuid.uuid4()
-
-    stream.add_callback(session['uid'], consume_tweet)
+    join_room('tweets')
     print('Client connected')
     emit('my response', {'data': 'Connected'})
-    
 
 @socketio.on('disconnect')
 def test_disconnect():
-    if('uid' in session):
-        stream.remove_callback(session['uid'])
+    leave_room('tweets')
     print('Client disconnected')
 
 if __name__ == '__main__':
--- twython_stream.py
+++ twython_stream.py
@@ -1,31 +1,18 @@
 from twython import TwythonStreamer
 import eventlet
 from eventlet.green import threading
+import socketio
 
 class TwitterStream(TwythonStreamer):
 
-    def __init__(self, consumer_key, consumer_secret, token, token_secret):
-        self.callbacks = dict()
-        self.lock = threading.Lock()
+    def __init__(self, consumer_key, consumer_secret, token, token_secret, callback):
+        self.callback = callback
         super(TwitterStream, self).__init__(consumer_key, consumer_secret, token, token_secret)
 
     def on_success(self, t):
-        if 'place' in t and t['place'] is not None:
-            print('Sending tweet')
-            with self.lock:
-                for callback in self.callbacks.values():
-                    callback(t)
-            eventlet.sleep(0.5)
+        self.callback(t)
         eventlet.sleep(0.1)
     
-    def add_callback(self, id, callback):
-        with self.lock:
-            self.callbacks[id] = callback
-
-    def remove_callback(self, id):
-        with self.lock:
-            del self.callbacks[id]
-
     def on_error(self, status_code, data):
         print(status_code)
         if status_code==420:
